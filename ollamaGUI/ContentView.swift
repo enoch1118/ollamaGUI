b207c4dbd2e45a93f317edfb3f9f29ee6febfc55
enoch <enoch4921@gmail.com>
2024-01-09 14:54:02 +0900

    Initial Commit

---
+++ ollamaGUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ollamaGUI
+//
+//  Created by 배상휘 on 1/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ollamaGUI/ollamaGUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ollamaGUIApp.swift
+//  ollamaGUI
+//
+//  Created by 배상휘 on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ollamaGUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
